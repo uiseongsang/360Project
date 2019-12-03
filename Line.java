813a1765590191a15d84254651e7c6b6d9e55b93
Ellimyst <sumukhvaze@gmail.com>
2019-12-03 14:55:21 -0700

    Add files via upload

--- Line.java
+++ Line.java
@@ -46,6 +46,127 @@ public class Line {
 		String str = origStr;
 		String front = "";
 		String end = "";
+		
+		//System.out.println(main +" , " +origStr);
+		//title
+		if(titleFont)
+		{
+			String s = "";
+			
+			for(int i = 0; i < str.length(); i++)
+			{
+				if(str.charAt(i) == ' ')
+				{
+					s += " ";
+				}
+				else
+				{
+					s += "-";
+				}
+			}
+			
+			str += "\n" + s;
+			//main = false;
+		}
+		
+		if(nextLine != null)
+		{
+			//System.out.println(getStr() +" : " +nextLine.getStr());
+			str += nextLine.getLine(false);
+		}
+		
+		if(main)
+		{
+			if(column)
+			{
+				lineSize = 80;
+				
+				String temp = "";
+				
+				for(int i = 0; i < str.length(); i++)
+				{
+					if(str.charAt(i) == '\n' && i > 0)
+					{
+						String space = "";
+						
+						for(int j = i; j%35 != 0; j++)
+						{
+							space += " ";
+						}
+						
+						str = str.substring(0, i) + space + str.substring(i+1);
+						
+						i += space.length() + 1;
+					}
+				}
+				
+				//divide and redistribute
+				String one = str.substring(0, str.length()/2);
+				String two = str.substring(str.length()/2);
+				
+				int curr = 0;
+				
+				while(curr < one.length())
+				{
+					String f = "";
+					String b = "";
+					
+					for(int i = 0; i < 35; i++)
+					{
+						if(curr + i < one.length())
+						{
+							f += one.charAt(curr + i);
+						}
+						
+						if(curr + i < two.length())
+						{
+							b += two.charAt(curr + i);
+						}
+					}
+					
+					String nl = "\n";
+					if(doubleSpaced)
+						nl += "\n";
+					
+					temp += f +"          " +b +nl;
+					
+					curr += 35;
+				}
+				
+				//if(doubleSpaced)
+				//temp += "\n";
+				str = temp;
+			}
+			else if(!titleFont)
+			{
+				//System.out.println("before: " +str);
+				int chars = 0;
+				
+				//System.out.print(lineSize);
+				for(int i = 0; i < str.length(); i++)
+				{
+					//System.out.print(chars);
+					if(chars == lineSize)
+					{
+						String nl = "\n";
+						if(doubleSpaced)
+							nl += "\n";
+						
+						String p = str.substring(i);
+						if(p.equals("\n"))
+							p = " ";
+						
+						str = str.substring(0, i) + nl + p;
+						
+						i += nl.length();
+						chars = 0;
+					}
+					
+					chars++;
+				}
+				
+			}
+		}
 
 		//left
 		if(align == 0)
@@ -75,140 +196,64 @@ public class Line {
 		//right
 		else if(align == 2)
 		{
-			int empty = lineSize-str.length();
-
-			for(int i = 0; i < empty; i++)
+			int start = 0;
+			
+			for(int i = str.length()-1; i > -1; i--)
 			{
-				front += " ";
+				if(str.charAt(i) == '\n')
+				{
+					start = i+1;
+					i = -1;
+				}
 			}
+
+			String space = "";
+			for(int i = 0; i < lineSize-(str.substring(start).length()); i++)
+			{
+				space += " ";
+			}
+			
+			str = str.substring(0, start) + space + str.substring(start);
 		}
 		else if(align == 3)
 		{
+			int start = 0;
 			
+			for(int i = str.length()-1; i > -1; i--)
+			{
+				if(str.charAt(i) == '\n')
+				{
+					start = i+1;
+					i = -1;
+				}
+			}
+
+			String s = str.substring(start);
+			String[] words = s.split("\\s");
+			if(words.length > 0)
+			{
+				int spaces = (lineSize - s.replaceAll("\\s", "").length())/words.length;
+				String space = "";
+				
+				for(int i = 0; i < spaces; i++)
+				{
+					space += " ";
+				}
+				
+				String m = "";
+				
+				m += space;
+				
+				for(String each : words)
+				{
+					m += each + space;
+				}
+				
+				str = str.substring(0, start) + m;
+			}
 		}
 
 		str = front + str + end;// + getSpace();
-
-		//System.out.println(main +" , " +origStr);
-		//title
-		if(titleFont)
-		{
-			String s = "";
-
-			for(int i = 0; i < str.length(); i++)
-			{
-				if(str.charAt(i) == ' ')
-				{
-					s += " ";
-				}
-				else
-				{
-					s += "-";
-				}
-			}
-
-			str += "\n" + s;
-			//main = false;
-		}
-
-		if(nextLine != null)
-		{
-			//System.out.println(getStr() +" : " +nextLine.getStr());
-			str += nextLine.getLine(false);
-		}
-
-		if(main)
-		{
-			if(column)
-			{
-				lineSize = 80;
-
-				String temp = "";
-
-				for(int i = 0; i < str.length(); i++)
-				{
-					if(str.charAt(i) == '\n' && i > 0)
-					{
-						String space = "";
-
-						for(int j = i; j%35 != 0; j++)
-						{
-							space += " ";
-						}
-
-						str = str.substring(0, i) + space + str.substring(i+1);
-
-						i += space.length() + 1;
-					}
-				}
-
-				//divide and redistribute
-				String one = str.substring(0, str.length()/2);
-				String two = str.substring(str.length()/2);
-
-				int curr = 0;
-
-				while(curr < one.length())
-				{
-					String f = "";
-					String b = "";
-
-					for(int i = 0; i < 35; i++)
-					{
-						if(curr + i < one.length())
-						{
-							f += one.charAt(curr + i);
-						}
-
-						if(curr + i < two.length())
-						{
-							b += two.charAt(curr + i);
-						}
-					}
-
-					String nl = "\n";
-					if(doubleSpaced)
-						nl += "\n";
-
-					temp += f +"          " +b +nl;
-
-					curr += 35;
-				}
-
-				//if(doubleSpaced)
-				//temp += "\n";
-				str = temp;
-			}
-			else if(!titleFont)
-			{
-				//System.out.println("before: " +str);
-				int chars = 0;
-
-				//System.out.print(lineSize);
-				for(int i = 0; i < str.length(); i++)
-				{
-					//System.out.print(chars);
-					if(chars == lineSize)
-					{
-						String nl = "\n";
-						if(doubleSpaced)
-							nl += "\n";
-
-						String p = str.substring(i);
-						if(p.equals("\n"))
-							p = " ";
-
-						str = str.substring(0, i) + nl + p;
-
-						i += nl.length();
-						chars = 0;
-					}
-
-					chars++;
-				}
-
-			}
-		}
 		if(nextLine == null)
 		{	
 			String nl = "\n";
